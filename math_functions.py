8e99c5ecdaaeff6ba2700b5f8736ad133310f49c
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-03 01:10:09 +0530

    Math functions

---
+++ math_functions.py
@@ -0,0 +1,9 @@
+import math
+
+x = math.ceil(2.9)
+print(x)
+y = 3.9
+print(round(y))
+
+# Math library implementation and use
+# See documentation here - https://docs.python.org/3/library/math.html
