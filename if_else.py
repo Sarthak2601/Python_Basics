d41cb25a2307575a767089d38cd976e845181d9f
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-03 01:22:56 +0530

    if-else statement

---
+++ if_else.py
@@ -0,0 +1,21 @@
+is_hot = False
+is_cold = False
+
+if is_hot :
+    print("It's a hot day")
+    print("Drink plenty of water")
+
+elif is_cold :
+    print("Its a cold day")
+    print("Wear warm clothes")
+else:
+    print("Its a lovely day" )
+
+# EXERCISE
+
+price = 1000000
+is_goodCredit = True
+if is_goodCredit :
+    print(f" Please put forward ${price * 0.1}")
+else:
+    print(f"Please put forward ${price * 0.2}")
