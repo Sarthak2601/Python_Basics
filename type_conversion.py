7a1e6c7e74a3651040ac99e6163f6fdcbc5d3c27
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-02 23:35:46 +0530

    exercise 3 : Type Conversion

---
+++ type_conversion.py
@@ -0,0 +1,9 @@
+#birth_year = int(input("Birth year : "))
+#age = 2020 - birth_year
+#print(age)
+
+weight = int(input("Enter your weight in kilograms"))
+weightgms = str( weight * 1000 )
+print('Weight in grams : ' + weightgms)
+
+# Exercise 3 : Type Conversion
