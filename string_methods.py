f47329adc63151c3725f4103f6c9c5896b4c21d1
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-03 00:35:21 +0530

    String methods

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+course = 'Python for Beginners'
+length = len(course)
+print(length)
+
+print(course.upper())  # Upper case - doesn't permanently change the var
+print(course.lower())  # Lower case
+
+print(course.find('n'))  # Returns index
+
+print(course.replace('Beginners','Absolute beginners '))  # Replaces Beginners with Absolute beginners
+
+print('Python ' in course)  # Checks the presence of the string in the variable | returns boolean value
