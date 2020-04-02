8764bf4a1f60e1509cbc8278446a16e52b082c20
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-03 00:47:56 +0530

    Arithmetic ops | Operator presedence

---
+++ arithmetic_operations.py
@@ -0,0 +1,14 @@
+print(10/3)  # returns decimal
+print(10//3)  # returns integer
+
+print (10 * 3)  #multiplication
+print (10 ** 3 ) #raised to the power 3
+
+
+# ORDER OF PRECEDENCE
+# parenthesis
+# exponent (**)
+# division or multiplication
+# addition or subtraction
+
+
