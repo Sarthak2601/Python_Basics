c1820dbfecce495fe66169b78dc3b5667300699d
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-02 23:16:52 +0530

    exercise 2 - input fn

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-patient = 'John Smith'
-age = 20
-isNewPatient = False
-if isNewPatient:
+patient = input('Enter your name' )
+age = input('Age?')
+isNewPatient = input('New Patient ?' )
+if isNewPatient == "No":
     print(patient)
     print(age)
 else:
-    print("Patient not present")
-# exercise 1
+    print("Patient not present" )
+# exercise 2 - Input
