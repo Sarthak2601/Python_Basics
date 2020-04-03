dff3231e9a1caca60d8ad659613ed17dc20e1333
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-04 00:12:27 +0530

    lists

---
+++ lists.py
@@ -0,0 +1,20 @@
+names = ['Rajeev', 'Anjali', 'Kriti', 'Sarthak']
+names[2] = 'Kittu'   # Updates list's elements
+print(names)
+
+# Exercise - Maximum number in list
+list = [2,4,3,6,7]
+maxNum = list[0]
+for i in list:
+    if i > maxNum:
+        maxNum = i
+print(maxNum)
+
+# 2D Lists
+
+lists = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print(lists)
