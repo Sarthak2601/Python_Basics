e11ab5ef78c140a0c7560fa5ea8f89c334b5ca6a
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-03 10:13:53 +0530

    comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,19 @@
+temp = 30
+if temp > 30 :
+    print('its a hot day')
+elif temp < 10 :
+    print('its a cold day')
+else:
+    print('its neither hot nor cold')
+
+
+# EXERCISE
+
+name = 'Sarthak'
+count = len(name)
+if count < 3 :
+    print('name must be at least 3 characters long')
+elif count > 50 :
+    print('name can be a maximum of 50 characters ')
+else:
+    print('name looks good')
