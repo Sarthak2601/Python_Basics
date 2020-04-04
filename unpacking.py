30c312c2995a16e511f688f0f5e65affdb6361dd
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-04 16:27:00 +0530

    Unpacking in Python

---
+++ unpacking.py
@@ -0,0 +1,13 @@
+# Using of shorthand to assign values of a list/tuple to variables
+
+# Example 1 - Tuples
+
+tuples = (1, 2, 3)
+x, y, z = tuples
+print(f"{x},{y},{z}")
+
+# Example 2 - Lists
+
+lists = [1, 2, 3]
+x, y, z = lists
+print(f"{x},{y},{z}")
