296944f7760cfdf8d5dc514ee294c9b522eb723d
Sarthak2601 <agarwal.sarthak262012@gmail.com>
2020-04-07 00:46:07 +0530

    Random function

---
+++ random_values.py
@@ -0,0 +1,8 @@
+import random
+
+print(random.random())  # Generates random values between 0 and 1
+print(random.randint(10, 20))  # Generates random values between the specified limits
+
+members = ["Rajeev", "Anjali", "Kriti", "Sarthak"]
+leader = random.choice(members)  # Picks a list element randomly
+print(f"The team leader is {leader}")
